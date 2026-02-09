a2b6bd4ff887435369743367ad5beab870bf0c8d
KapitalSP <ekdrms0805@gmail.com>
2026-02-09 18:09:15 +0900

    RUN.sh

--- RUN.sh
+++ RUN.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-python3 void.py
+echo " [VOID] Mobile Bootloader..."
+echo " Checking Dependencies..."
+pkg install -y python llama-cpp > /dev/null 2>&1
+echo " Launching Core..."
+python void.py
