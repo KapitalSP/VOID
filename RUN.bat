a766ec6c56bc76ef084d4068b6c131b87fc836f3
KapitalSP <ekdrms0805@gmail.com>
2026-02-09 18:08:56 +0900

    RUN.bat

--- RUN.bat
+++ RUN.bat
@@ -1,5 +1,9 @@
 @echo off
-title PROJECT VOID
-echo Initializing System...
+title VOID: Auto-Bootloader
+color 0F
+echo.
+echo  [ VOID System Ignition ]
+echo  Loading Neural Engine...
+echo.
 python void.py
 pause
